f2fb16936b3abf748d680761e18fb5d980242258
Pedro Manoel Evangelista <pevange@thoughtworks.com>
2020-04-18 12:38:36 -0300

    Save latest changes

---
+++ src/test/java/codes/pedromanoel/learning/rxjava/SchedulersTest.java
@@ -0,0 +1,11 @@
+package codes.pedromanoel.learning.rxjava;
+
+import io.reactivex.schedulers.Schedulers;
+import org.junit.jupiter.api.Test;
+
+public class SchedulersTest {
+
+    @Test
+    void name() {
+    }
+}
